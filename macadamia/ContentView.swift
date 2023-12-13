89502c03dcfb0fb7deb2bc945de5263ef4295c18
zeugmaster <zeugmaster@protonmail.com>
2023-12-13 13:57:34 +0100

    Initial Commit

---
+++ macadamia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macadamia
+//
+//  Created by Dario Lass on 13.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macadamia/macadamiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  macadamiaApp.swift
+//  macadamia
+//
+//  Created by Dario Lass on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct macadamiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
