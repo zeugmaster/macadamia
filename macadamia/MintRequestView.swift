bd6dad8ebd44c4b8195c00916a54f7061f2178ff
zeugmaster <zeugmaster@protonmail.com>
2023-12-15 19:38:39 +0100

    got rudimentary mint flow working

--- macadamia/MintRequestView.swift
+++ macadamia/MintRequestView.swift
@@ -20,7 +20,7 @@ struct MintRequestView: View {
                         .monospaced()
                     Text("sats")
                 }
-                Picker("Mint", selection:$viewmodel.selectedMint) {
+                Picker("Mint", selection:$viewmodel.selectedMintString) {
                     ForEach(viewmodel.mintList, id: \.self) {
                         Text($0)
                     }
@@ -32,7 +32,7 @@ struct MintRequestView: View {
         .navigationTitle("Mint")
         .navigationBarTitleDisplayMode(.inline)
         Spacer()
-        NavigationLink(destination: MintRequestInvoiceView()) {
+        NavigationLink(destination: MintRequestInvoiceView(viewmodel: viewmodel)) {
             Text("Request \(Image(systemName: "bolt.fill")) Invoice")
                 .frame(maxWidth: .infinity)
                 .padding()
@@ -42,7 +42,7 @@ struct MintRequestView: View {
         .buttonStyle(.bordered)
         .padding()
         .toolbar(.hidden, for: .tabBar)
-        .disabled(viewmodel.numberString.isEmpty)
+        .disabled(viewmodel.numberString.isEmpty || viewmodel.amount == 0)
     }
 }
 
@@ -50,78 +50,134 @@ struct MintRequestView: View {
     MintRequestView()
 }
 
-class MintRequestViewModel: ObservableObject {
-    @Published var numberString: String = ""
-    @Published var mintList:[String] = [""]
-    @Published var selectedMint:String = ""
-    
-    private var wallet = Wallet.shared
-    
-    var amount: Int? {
-        return Int(numberString)
-    }
-    
-    func fetchMintInfo() {
-        print("fetch")
-        for mint in wallet.database.mints {
-            mintList = []
-            let readable = mint.url.absoluteString.dropFirst(8)
-            mintList.append(String(readable))
-        }
-        selectedMint = mintList[0]
-    }
-}
 
 //MARK: -
 
 struct MintRequestInvoiceView: View {
-    //@StateObject var viewmodel = MintRequestViewModel()
-    
-    @State private var text = "cashuAeialwurt0984u3q0üjpifjpq083u4tüpifjr3"
+    @ObservedObject var viewmodel:MintRequestViewModel
     
     var body: some View {
         VStack {
             Spacer()
             HStack {
-                TextField("Enter text", text: $text)
+                TextField("", text: $viewmodel.invoiceString)
                     .textFieldStyle(PlainTextFieldStyle())
                     .dynamicTypeSize(.xxLarge)
                     .disabled(true)
                     .foregroundColor(.secondary)
                     .monospaced()
-                Button(action: copyToClipboard) {
+                    .multilineTextAlignment(.center)
+                Button(action: viewmodel.copyToClipboard) {
                     Image(systemName: "list.clipboard")
                         .dynamicTypeSize(.xxxLarge)
                 }
+                .disabled(viewmodel.loadingInvoice)
             }
             .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40
                                ))
-            
-            QRCodeView(qrCode: generateQRCode(from: "lnbc210n1pjhkfs0pp5qh2zr8w7vhjfvazgk2f06nv9y8j0fvrye9kz2yf0fpj2muzs35vscqpjsp5te6ukpn9xpcrktv0wvah3pn0lkcv9ks46hl22u9hvkc28wvmfqcs9q7sqqqqqqqqqqqqqqqqqqqsqqqqqysgqdqqmqz9gxqyjw5qrzjqwryaup9lh50kkranzgcdnn2fgvx390wgj5jd07rwr3vxeje0glcllezhk2zechxl5qqqqlgqqqqqeqqjq9yuxv8tagxhtgqkjclc8sc6xxkcxhxdy8m4nn0kjxl04sux2032swckkxc0hj3c5llm5c5pkvqg28mvqy50v5ana6zdkd6zstf46vhsphnxtw3"))
-                .padding(40)
+            if viewmodel.loadingInvoice {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .padding(80)
+                    .scaleEffect(CGSize(width: 2.0, height: 2.0))
+            } else if viewmodel.invoiceString != "loading..." {
+                QRCodeView(qrCode: generateQRCode(from: viewmodel.invoiceString))
+                    .padding(40)
+            }
                 
             Spacer()
-            NavigationLink(destination: Text("bing, like a rocketship")) {
+            NavigationLink(destination: MintRequestCompletionView(viewModel: viewmodel)) {
                 Text("I have paid the \(Image(systemName: "bolt.fill")) Invoice")
                     .frame(maxWidth: .infinity)
                     .padding()
                     .bold()
                     .foregroundColor(.white)
-                    
             }
             .buttonStyle(.bordered)
             .padding()
             .toolbar(.hidden, for: .tabBar)
+            .disabled(viewmodel.loadingInvoice)
         }
-        
+        .onAppear(perform: {
+            viewmodel.requestQuote()
+        })
     }
+}
+
+struct MintRequestCompletionView: View {
+    @ObservedObject var viewModel: MintRequestViewModel
     
-    private func copyToClipboard() {
-        UIPasteboard.general.string = text
+    var body: some View {
+        Text(viewModel.mintRequestState).onAppear(perform: {
+            viewModel.requestMinting()
+        })
+            
     }
-    
 }
 
 #Preview {
-    MintRequestInvoiceView()
+    MintRequestInvoiceView(viewmodel: MintRequestViewModel())
+}
+
+
+@MainActor
+class MintRequestViewModel: ObservableObject {
+    @Published var numberString: String = ""
+    @Published var mintList:[String] = [""]
+    @Published var selectedMintString:String = ""
+    @Published var loadingInvoice = true
+    @Published var invoiceString = "loading..."
+    @Published var errorToDisplay:Error?
+    @Published var mintRequestState = "loading..."
+    
+    private var wallet = Wallet.shared
+    private var selectedMint:Mint?
+    private var quote:QuoteRequestResponse?
+    
+    var amount: Int {
+        return Int(numberString) ?? 0
+    }
+    
+    func fetchMintInfo() {
+        for mint in wallet.database.mints {
+            mintList = []
+            let readable = mint.url.absoluteString.dropFirst(8)
+            mintList.append(String(readable))
+        }
+        selectedMintString = mintList[0]
+    }
+    
+    func requestQuote() {
+        print("lezgo")
+        
+        loadingInvoice = true
+        selectedMint = wallet.database.mints.first(where: {$0.url.absoluteString.contains(selectedMintString)})!
+        
+        Task {
+            do {
+                quote = try await wallet.getQuote(from:selectedMint!, for:amount)
+                invoiceString = quote!.pr
+                loadingInvoice = false
+                
+            } catch {
+                errorToDisplay = error
+            }
+            
+        }
+    }
+    
+    func copyToClipboard() {
+        UIPasteboard.general.string = invoiceString
+    }
+    
+    func requestMinting() {
+        Task {
+            do {
+                try await wallet.requestMint(from:selectedMint!,for:quote!,with:amount)
+                mintRequestState = "Success"
+            } catch {
+                mintRequestState = error.localizedDescription
+            }
+        }
+    }
 }
