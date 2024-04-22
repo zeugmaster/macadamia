e590167bb3703d77ea88d0774b53487178260f57
zeugmaster <zeugmaster@protonmail.com>
2024-04-22 23:49:03 +0200

    remove old scanner code commented

--- macadamia/QRScanner.swift
+++ macadamia/QRScanner.swift
@@ -1,18 +1,96 @@
+////
+////  QRScanner.swift
+////  macadamia
+////
+////  Created by zm on 21.04.24.
+////
 //
-//  QRScanner.swift
-//  macadamia
-//
-//  Created by zm on 21.04.24.
-//
-
 import SwiftUI
+import URUI
+import Combine
 
 struct QRScanner: View {
+    @ObservedObject var viewModel: QRScannerViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment:.bottom) {
+            if viewModel.isScanning {
+                URVideo(videoSession: viewModel.videoSession)
+                    
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color.clear)
+                    .background(Color.black.opacity(0.5))
+                Text("\(Int(viewModel.estimatedPercentComplete * 100))%")
+                    .padding()
+            }
+            .frame(maxHeight: 40)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 6.0))
     }
 }
 
-#Preview {
-    QRScanner()
+
+@MainActor
+class QRScannerViewModel: ObservableObject {
+    let codesPublisher = URCodesPublisher()
+    @Published var videoSession: URVideoSession
+    @Published var scanState: URScanState
+    @Published var estimatedPercentComplete = 0.0
+    @Published var fragmentStates = [URFragmentBar.FragmentState]()
+    @Published var result: URScanResult?
+    @Published var isScanning = true
+    @Published var hrRes: String?
+
+    var onResult: ((String) -> Void)?
+
+    init() {
+        videoSession = URVideoSession(codesPublisher: codesPublisher)
+        scanState = URScanState(codesPublisher: codesPublisher)
+        subscribeToScanState()
+    }
+
+    func subscribeToScanState() {
+        scanState.resultPublisher.sink { [weak self] result in
+            self?.handleScanResult(result: result)
+        }.store(in: &cancellables)
+    }
+
+    func handleScanResult(result: URScanResult) {
+        // Implement the result handling logic, moving it from the QRScanner view
+        
+        switch result {
+        case .failure(_):
+            print("failure")
+            self.estimatedPercentComplete = 0
+        case .other(_): 
+            print("undef scan res")
+        case .progress(let progress): 
+            self.estimatedPercentComplete = progress.estimatedPercentComplete
+        case .reject: 
+            print("rejected")
+        case .ur(let ur):
+            if case .bytes(let urBytes) = ur.cbor {
+                if let string = String(data: urBytes, encoding: .utf8),
+                   let onResult = onResult {
+                    onResult(string)
+                    isScanning = false
+                    estimatedPercentComplete = 1
+                } else {
+                    restart()
+                }
+            }
+        }
+    }
+
+    func restart() {
+        result = nil
+        estimatedPercentComplete = 0
+        fragmentStates = [.off]
+        isScanning = true
+        scanState.restart()
+    }
+
+    private var cancellables = Set<AnyCancellable>()
 }
