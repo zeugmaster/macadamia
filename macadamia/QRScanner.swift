6691518169896b9c17445a5c211918e69396bc90
zeugmaster <zeugmaster@protonmail.com>
2024-04-22 23:45:18 +0200

    first attempt at animated QR Scanner

---
+++ macadamia/QRScanner.swift
@@ -0,0 +1,18 @@
+//
+//  QRScanner.swift
+//  macadamia
+//
+//  Created by zm on 21.04.24.
+//
+
+import SwiftUI
+
+struct QRScanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRScanner()
+}
