456da6714c005e6185a7f9a84c62317ac7f3b7b2
zeugmaster <zeugmaster@protonmail.com>
2024-07-21 16:41:58 +0200

    add improved balance text view, mint manager mock up

---
+++ macadamia/BalanceView.swift
@@ -0,0 +1,18 @@
+//
+//  BalanceView.swift
+//  macadamia
+//
+//  Created by zm on 21.07.24.
+//
+
+import SwiftUI
+
+struct BalanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BalanceView()
+}
---
+++ macadamia/MintDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MintDetailView.swift
+//  macadamia
+//
+//  Created by zm on 18.07.24.
+//
+
+import SwiftUI
+
+struct MintDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MintDetailView()
+}
