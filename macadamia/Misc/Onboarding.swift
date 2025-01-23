4bd944785b02c7e287d75cf30ae0230a027ebd50
Dario <zeugmaster@protonmail.com>
2025-01-23 18:48:20 +0100

    add Onboarding

---
+++ macadamia/Misc/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  macadamia
+//
+//  Created by zm on 23.01.25.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
