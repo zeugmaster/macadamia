beab47d4a7db3630acda5358a570ae6068048903
Dario <zeugmaster@protonmail.com>
2026-01-13 18:54:15 +0200

    add empty lnurlpay view

---
+++ macadamia/Wallet/Pay/LNURLPayView.swift
@@ -0,0 +1,18 @@
+//
+//  LNURLPayView.swift
+//  macadamia
+//
+//  Created by zm on 13.01.26.
+//
+
+import SwiftUI
+
+struct LNURLPayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LNURLPayView()
+}
