161039ff8740250aa50797c0a38913007acfb53e
Dario <zeugmaster@protonmail.com>
2026-09-15 13:23:42 +0200

    payment method card

---
+++ macadamia/Wallet/Pay/PaymentMethods.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentMethods.swift
+//  macadamia
+//
+//  Created by zm on 15.09.26.
+//
+
+import SwiftUI
+
+struct PaymentMethods: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentMethods()
+}
